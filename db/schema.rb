a3a880582cd7e202413fee64a4ea1ae0c3619068
Mikito Yoshiya <mikito0521@gmail.com>
2013-11-24 13:41:26 +0900

    appモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131124042453) do
+ActiveRecord::Schema.define(version: 20131124043610) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -45,5 +45,14 @@ ActiveRecord::Schema.define(version: 20131124042453) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "apps", force: true do |t|
+    t.string   "name",       null: false
+    t.text     "files"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "apps", ["name"], name: "index_apps_on_name", unique: true, using: :btree
 
 end
